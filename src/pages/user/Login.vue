d6091e73386a33516591742483f7ad6237d7d84c
chenghongqiang <962691576@qq.com>
2019-01-04 13:48:42 +0800

    登录页

---
+++ src/pages/user/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login_page">
+    <section class="form_contianer">
+      <div class="manage_tip">
+        <p>第一个后台管理系统</p>
+      </div>
+      <el-form>
+        <el-form-item prop="username">
+          <el-input placeholder="用户名"><span>dsfsf</span></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" placeholder="密码" ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="submit_btn">登陆</el-button>
+        </el-form-item>
+      </el-form>
+    </section>
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style scoped>
+  @import '../../assets/css/login.css';
+</style>
